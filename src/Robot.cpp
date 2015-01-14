6819c19348a485de63bdd5e501a3ea6c38e871ae
bigbrain890 <bigbrain890@yahoo.com>
2015-01-13 21:13:04 -0500

    Addition of AnalogInput
    Added objects for an ultra sonic sensor, and a potentiometer. Note:
    AnalogInput has replaced AnalogChannel

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -2,20 +2,26 @@
 
 /**
  * This is the basic code for a mecanum drive robot. All it does is drive a macanum chassis. Field centric mecanum
- * is not supported as I'm not using a gyro for the 4th argument of the chassis object.
+ * is not supported as I'm not using a gyro for the 4th argument of the chassis object. Also, the AnalogChannel class
+ * been replaced with AnalogInput.
  */
 class Robot: public SampleRobot
 {
 	RobotDrive chassis; // Object for robot movement.
 	Joystick gamepad; // Logitech Dual Action for drive control.
 	Joystick manipulator; // Object for later arm manipulator.
+	AnalogInput ultraSonic; //eventual ultrasonic sensor for distance measuring
+	AnalogInput pot; 		  // Potentiometer for positioning of eveutual forklift
 	float chassisGyro = 0.0; //Arbitrary gyro value for 4th float gyro value of mecanum cartesian drive
 
 public:
 	Robot() :
 			chassis(0, 1, 2, 3),	// 4 PWM channels for control of drive motors.
-			gamepad(0),		// Gamepad USB plug
-			manipulator(1)	// Manipulator USB plug
+			gamepad(0),				// Gamepad USB plug
+			manipulator(1),			// Manipulator USB plug
+			ultraSonic (0), 		// Sensor feeding data into Analog input port 0
+			pot (1) 				//Sensor feeding data into Analog input port 1
+
 	{
 		chassis.SetExpiration(0.1);
 	}
