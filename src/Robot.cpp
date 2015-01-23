bd6bdc0cea9b6a5d3d5b9789ab4302b78686890a
bigbrain890 <bigbrain890@yahoo.com>
2015-01-23 08:40:10 -0500

    Mecannum drive driving
    Added some more comments and inverted front two wheels on the drive
    system. They were driving backwards on the practice chassis.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -13,7 +13,7 @@ class Robot: public SampleRobot
 	Joystick manipulator; // Object for later arm manipulator.
 	AnalogInput ultraSonic; //eventual ultrasonic sensor for distance measuring
 	AnalogInput pot; 		  // Potentiometer for positioning of eveutual forklift
-	float chassisGyro = 0.0; //Arbitrary gyro value for 4th float gyro value of mecanum cartesian drive
+	const float chassisGyro = 0.0; //Arbitrary gyro value for 4th float gyro value of mecanum cartesian drive
 	float distanceVoltage; // voltage from ultrasonic
 	float distance;		   // Distance in inches using scaler factor on distanceVoltage
 	float roundedDistance; // Rounded number for inches to get rid of decimal places.
@@ -45,21 +45,25 @@ public:
 		chassis.SetSafetyEnabled(true);
 		while (IsOperatorControl() && IsEnabled())
 		{
+			chassis.SetInvertedMotor(chassis.kFrontLeftMotor, true); // Invert front left motor.
+			chassis.SetInvertedMotor(chassis.kFrontRightMotor, true);// Invert front right motor.
 			chassis.MecanumDrive_Cartesian(gamepad.GetRawAxis(1), gamepad.GetRawAxis(2), gamepad.GetRawAxis(3), chassisGyro);
 			Wait(0.0005); // wait for a motor update time
 			distanceVoltage = ultraSonic.GetVoltage(); // reading raw voltage from ultra sonic into variable distanceVoltage
-			oldAngle = angle;
+			oldAngle = angle;		  // Take the last angle and store it as the old angle
 			angle = pot.GetVoltage(); // read voltage in from pot and store into variable angle.
 			distance = distanceVoltage / 0.00977;	   // converting voltage to inches with scaler
 			roundedDistance = ceilf(distance * 100) / 100; // Doing calculations to round distance value.
 			SmartDashboard::PutNumber("Distance", roundedDistance); // Print the distance to SmartDashboard (not so smart).
-			upperLim = oldAngle + 0.01;
-			lowerLim = oldAngle - 0.01;
-			if ((angle > upperLim) || (angle < lowerLim)) {
-				SmartDashboard::PutNumber("Angle", angle);
+			upperLim = oldAngle + 0.01;		// Set an upper limit to compare new angle to
+			lowerLim = oldAngle - 0.01;		// Set a lower limit to compare new angle to
+			if ((angle > upperLim) || (angle < lowerLim))	// If angle has actually changed a considerable amount...
+			{
+				SmartDashboard::PutNumber("Angle", angle);  // Print it out to the dashboard.
 			}
-			if ((angle < upperLim) && (angle > lowerLim)) {
-				Wait(0.005);
+			if ((angle < upperLim) && (angle > lowerLim))   // If it hasn't...
+			{
+				Wait(0.005);								// Don't do anything
 			}
 
 		}
