384394d439ba19d7f3371f0569c1e4d5012ad08f
bigbrain890 <bigbrain890@yahoo.com>
2015-02-05 13:52:22 -0500

    Controls systems for Manipulator
    Getting devices started for forklift and sensors.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -5,6 +5,7 @@
 #define LEFT_STICK_X_AXIS 0
 #define LEFT_STICK_Y_AXIS 1
 
+
 /**
  * Author: Ethan Novilla
  * Team: #3641 The Flying Toasters
@@ -22,10 +23,16 @@ class Robot: public SampleRobot
 	Joystick gamepad; 				// Logitech Dual Action for drive control.
 	Joystick manipulator;   		// Object for later arm manipulator.
 	AnalogInput ultraSonic; 		// Eventual ultrasonic sensor for distance measuring.
-	AnalogInput pot; 	  			// Potentiometer for positioning of eveutual forklift.
-	Solenoid hDrive;				// Pneumatic cilender for deploying H drive system.
-	Talon hDriveMotor;				// Motor running on H drive system.
+	AnalogInput lowerStagePot; 	  	// Potentiometer for positioning of lower forklift stage.
+	AnalogInput upperStagePot;		// Potentiometer for positioning of upper forklift stage.
+	Talon hDriveMotor;				// Splitter running 2 motors on H drive system.
+	Talon lowerStage;				// Motor for running the lower stage of the fork lift.
+	Talon upperStage;				// Motor for running the upper stage of the forck lift.
+	Talon intakeMotor;				// Splitter running 2 motors on intake arm.
+	DoubleSolenoid rightIntake;		// Actuating right intake armin into place.
+	DoubleSolenoid leftIntake;		// Actuating left intake arm into place.
 	PowerDistributionPanel pdBoard;	// pdBoard used for hardware debug
+	std::ostringstream printDis;	// Object used to
 	float distanceVoltage;			// voltage from ultrasonic.
 	float distance;		  			// Distance in inches using scaler factor on distanceVoltage.
 	float roundedDistance;			// Rounded number for inches to get rid of decimal places.
@@ -40,10 +47,15 @@ public:
 			chassis(0, 2, 1, 3),	// 4 PWM channels for control of drive motors.
 			gamepad(0),				// Gamepad USB plug.
 			manipulator(1),			// Manipulator USB plug.
-			ultraSonic (0), 		// Sensor feeding data into Analog input port 0.
-			pot (1), 				// Sensor feeding data into Analog input port 1.
-			hDrive(0),				// Solenoid for hDrive is on port 0 of the pneumatic controller.
-			hDriveMotor(4)			// H drive motor.
+			lowerStagePot (1), 		// Sensor feeding data into Analog input port 1.
+			upperStagePot (2),		// Sensor feeding data into Analog input port 2.
+			hDriveMotor(4),			// H drive motor.
+			lowerStage(6),
+			upperStage(7),
+			intakeMotor(5),			// 2 motors feeding off of pwm output 5
+			rightIntake(0),
+			leftIntake(1)
+
 	{
 		chassis.SetExpiration(0.1);
 	}
@@ -59,27 +71,20 @@ public:
 		chassis.SetSafetyEnabled(true);
 		while (IsOperatorControl() && IsEnabled())
 		{
-			chassis.ArcadeDrive(gamepad.GetRawAxis(1), gamepad.GetRawAxis(RIGHT_STICK_X_AXIS)*-1);
+			chassis.ArcadeDrive(gamepad.GetRawAxis(LEFT_STICK_Y_AXIS), gamepad.GetRawAxis(RIGHT_STICK_X_AXIS)*-1);
 			while (gamepad.GetRawButton(6) == true)
 			{
-				chassis.ArcadeDrive(gamepad.GetRawAxis(1), gamepad.GetRawAxis(RIGHT_STICK_X_AXIS)*-1);
+				chassis.ArcadeDrive(gamepad.GetRawAxis(LEFT_STICK_Y_AXIS), gamepad.GetRawAxis(RIGHT_STICK_X_AXIS)*-1);
 				hDriveMotor.Set(gamepad.GetRawAxis(LEFT_STICK_X_AXIS)*-1);
 
 			}
 			hDriveMotor.Set(0);
 			Wait(0.0005);
-			/*while(gamepad.GetRawButton(6) == true)							// While I'm pressing button 6.
-			{
-				hDrive.Set(true);											// Engage actuator contacting h drive wheel with ground.
-				hDriveMotor.Set(gamepad.GetRawAxis(2)*-1);						// Run H drive motor referencing x movement on right stick.
-			}
-			*/
 			distanceVoltage = ultraSonic.GetVoltage(); 						// Reading raw voltage from ultra sonic into variable distanceVoltage.
-			oldAngle = angle;		  										// Take the last angle and store it as the old angle.
-			angle = pot.GetVoltage(); 										// Read voltage in from pot and store into variable angle.
 			distance = distanceVoltage / 0.00977;	   						// Converting voltage to inches with scaler.
 			roundedDistance = ceilf(distance * 100) / 100; 					// Doing calculations to round distance value.
-			SmartDashboard::PutNumber("Distance", roundedDistance); 		// Print the distance to SmartDashboard (not so smart).
+			SmartDashboard::PutString("DB/String 0", printDis.str().c_str());
+			oldAngle = angle;		  										// Take the last angle and store it as the old angle.
 			upperLim = oldAngle + 0.01;										// Set an upper limit to compare new angle to
 			lowerLim = oldAngle - 0.01;										// Set a lower limit to compare new angle to
 			if ((angle > upperLim) || (angle < lowerLim))					// If angle has actually changed a considerable amount...
@@ -96,60 +101,18 @@ public:
 
 	void Test()
 	{
-#if 1
+
 #define PORT_COUNT 16
 		double ports[PORT_COUNT];
 		for (int i = 0; i < PORT_COUNT; i++)
 			ports[i] = pdBoard.GetCurrent(i);
-		for (int i = 0; i < PORT_COUNT; i++) {
-#if 1
+		for (int i = 0; i < PORT_COUNT; i++)
+		{
 			std::ostringstream strstrm;
 			strstrm << "Current Draw " << i << ": ";
 			SmartDashboard::PutNumber(strstrm.str().c_str(), ports[i]);
-
-#else
-			char msg[64];
-			sprintf(msg, "Current Draw %d: ", i);
-			SmartDashboard::PutNumber(msg, ports[i]);	// Print that value out to the Smart Dasboard for Debug
-#endif
 		}
-#else
-		double port0 = pdBoard.GetCurrent(0);	//Read a particular current draw from a port and store it into a variable.
-		double port1 = pdBoard.GetCurrent(1);
-		double port2 = pdBoard.GetCurrent(2);
-		double port3 = pdBoard.GetCurrent(3);
-		double port4 = pdBoard.GetCurrent(4);
-		double port5 = pdBoard.GetCurrent(5);
-		double port6 = pdBoard.GetCurrent(6);
-		double port7 = pdBoard.GetCurrent(7);
-		double port8 = pdBoard.GetCurrent(8);
-		double port9 = pdBoard.GetCurrent(9);
-		double port10 = pdBoard.GetCurrent(10);
-		double port11 = pdBoard.GetCurrent(11);
-		double port12 = pdBoard.GetCurrent(12);
-		double port13 = pdBoard.GetCurrent(13);
-		double port14 = pdBoard.GetCurrent(14);
-		double port15 = pdBoard.GetCurrent(15);
-
-		SmartDashboard::PutNumber("Current Draw 0: ", port0);	// Print that value out to the Smart Dasboard for Debug
-		SmartDashboard::PutNumber("Current Draw 1: ", port1);
-		SmartDashboard::PutNumber("Current Draw 2: ", port2);
-		SmartDashboard::PutNumber("Current Draw 3: ", port3);
-		SmartDashboard::PutNumber("Current Draw 4: ", port4);
-		SmartDashboard::PutNumber("Current Draw 5: ", port5);
-		SmartDashboard::PutNumber("Current Draw 6: ", port6);
-		SmartDashboard::PutNumber("Current Draw 7: ", port7);
-		SmartDashboard::PutNumber("Current Draw 8: ", port8);
-		SmartDashboard::PutNumber("Current Draw 9: ", port9);
-		SmartDashboard::PutNumber("Current Draw 10: ", port10);
-		SmartDashboard::PutNumber("Current Draw 11: ", port11);
-		SmartDashboard::PutNumber("Current Draw 12: ", port12);
-		SmartDashboard::PutNumber("Current Draw 13: ", port13);
-		SmartDashboard::PutNumber("Current Draw 14: ", port14);
-		SmartDashboard::PutNumber("Current Draw 15: ", port15);
-#endif
-		}
-
+	}
 };
 
 START_ROBOT_CLASS(Robot);
